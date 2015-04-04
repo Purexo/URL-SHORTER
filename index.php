d28770a421ff9e00d430fe77dd9dfd47375729ef
Purexo <contact@purexo.eu>
2015-04-04 17:40:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ redirect();
     </head>
     <body>
 
-        <nav class="navbar navbar-inverse">
+        <nav class="navbar-inverse">
             <div class="container">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
